fc57a5b675d71f3f532abfc77fcd56f21f36a9b6
ranjithravi9 <150169990+ranjithravi9@users.noreply.github.com>
2023-11-07 21:53:44 +0530

    Add files via upload

---
+++ admin/menu.php
@@ -0,0 +1,28 @@
+
+<?php
+	session_start();
+	if(!$_SESSION['uname'] && (!$_SESSION['pass'])){
+		header("location: ../login.php");
+	}
+?>
+<div id="top">
+			
+			<div id="top-navigation">
+				Welcome <a href="#"><strong>Administrator</strong></a>
+				<span>|</span>
+				<a href="support.html">Help</a>
+				<span>|</span>
+				<a href="#">Profile Settings</a>
+				<span>|</span>
+				<a href="logout.php">Log out</a>
+			</div>
+		</div>
+<div id="navigation">
+			<ul>
+			    <li><a href="index.php"><span>Dashboard</span></a></li>
+			    <li><a href="add_vehicles.php"><span>Vehicle Management</span></a></li>
+			    <li><a href="client_requests.php"><span>Hire Requests</span></a></li>
+			    <li><a href="index.php"><span>Messages</span></a></li>
+			  
+			</ul>
+		</div>
---
+++ header.php
@@ -0,0 +1,26 @@
+
+<header>
+			<div class="wrapper">
+				<h1 class="logo"> Car Company</h1>
+				<a href="#" class="hamburger"></a>
+				<nav>
+					
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="index.php">Rent Cars</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+					<a href="account.php">Client Login</a>
+					<a href="login.php">Admin Login</a>
+					
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="status.php">View Status</a></li>
+								<li><a href="message_admin.php">Message Admin</a></li>
+							</ul>
+					<a href="admin/logout.php">Logout</a>
+					
+				</nav>
+			</div>
+		</header>
